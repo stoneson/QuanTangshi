f930a3c4f0c433f1f01611c71598e7e68e04b12a
animalize <malincns@163.com>
2017-08-08 23:46:59 +0800

    【】重构

--- app/src/main/java/animalize/github/com/quantangshi/UIPoem/TagView.java
+++ app/src/main/java/animalize/github/com/quantangshi/UIPoem/TagView.java
@@ -138,12 +138,7 @@ public class TagView extends LinearLayout implements View.OnClickListener, co.lu
                 TagView.this.removeTag(info);
             }
         });
-        builder.setNegativeButton("返回", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            }
-        });
+        builder.setNegativeButton("取消", null);
         builder.show();
     }
 }
